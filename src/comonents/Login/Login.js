a19830e39e36cf84fdbfba0e343c7120eeee0834
Piko327 <pikopikop372@gmail.com>
2023-01-26 09:21:54 +0200

    get data from login

--- src/comonents/Login/Login.js
+++ src/comonents/Login/Login.js
@@ -2,10 +2,21 @@ import { Link } from "react-router-dom"
 
 export const Login=()=>
 {
+   const onSubmitHandler=(e)=>
+   {
+    e.preventDefault()
+
+
+    let data=Object.fromEntries(new FormData(e.target))
+    let email= data.email
+    let password= data.password
+  
+   }
+
     return <section id="login">
     <div className="form">
       <h2>Login</h2>
-      <form class="login-form">
+      <form class="login-form" onSubmit={onSubmitHandler}>
         <input type="text" name="email" id="email" placeholder="email" />
         <input type="password" name="password" id="password" placeholder="password" />
         <button type="submit">login</button>
