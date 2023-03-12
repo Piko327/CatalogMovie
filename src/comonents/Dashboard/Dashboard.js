43380aebc80973adba301422ba8a8e8dbdad8c1b
Piko327 <pikopikop372@gmail.com>
2023-03-12 21:40:04 +0200

    responsive header

--- src/comonents/Dashboard/Dashboard.js
+++ src/comonents/Dashboard/Dashboard.js
@@ -6,25 +6,19 @@ import { Card  } from "./Card";
 const Dashboard =()=>
 {
   
-const isEmpty =(obj) =>Object.keys(obj).length === 0;
+const isEmpty=(obj) =>Object.keys(obj).length===0;
  const {data,setInfo} = useContext(DataContext)
 
   useEffect(() => {
     getAll()
       .then(data =>setInfo(data))
       ;}, []);
-
-  
     return <section id="dashboard">
-   {data ?
-    <> <h2>ALBUMS</h2>
+   {!isEmpty(data)?
+    <> 
+    <h2>ALBUMS</h2>
     <ul className="card-wrapper">
-      
-     {isEmpty(data)
-     ? null
-     :
-     data.map((x)=><Card  key={x._id}  value={x}/>)}
-    
+     {data.map((x)=><Card  key={x._id}  value={x}/>)}
     </ul>
     </>
    : <h2>There are no albums added yet.</h2>}
--- src/comonents/Header/Header.js
+++ src/comonents/Header/Header.js
@@ -1,42 +1,47 @@
 import { Link } from "react-router-dom"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
 
 const Header =()=>{
     
-  const {setLogin,auth} =useContext(AuthContext)
+  const {auth} =useContext(AuthContext)
+
+  //du=inamic
+  const [visible, setVisible] = useState(true);
+
+  const dynamicNavbar = (e) => {
+    if (e.pageX<=935 && visible === true)
+    setVisible(false) 
+     else 
+     setVisible(true);
+  };
 
   function isEmpty(obj) {
     return Object.keys(obj).length === 0;
 }
-
-    return  <header>
+    return <div className={visible ? "closed":"opened"} >
+     <header className={visible ?"header_nav active":"header_nav " }>
     <nav>
+  <button className="material-symbols-outlined"  onClick={dynamicNavbar}>
+         {visible ? "menu":"close"}</button>
     <div>
   <Link to="/Dashboard">Dashboard</Link>
 </div>
-
 {isEmpty(auth)
-?    <>
+?   
 <div className="guest">
         <Link to="/Login">Login</Link>
         <Link to="/Register">Register</Link>
       </div>
-</>
 :
-
 <div className="user">
 <Link to="/Create">Add Album</Link>
 <Link to="/Logout">Logout</Link>
 </div>
-
 }
-
-
-
     </nav>
   </header>
-
+</div>
     
 }
 
