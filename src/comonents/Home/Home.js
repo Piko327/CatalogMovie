ab7dd58c0a7682e6b3a9c4c118e719d9dd9f01c8
Piko327 <pikopikop372@gmail.com>
2023-03-16 13:56:31 +0200

    home page test

--- src/comonents/Home/Home.js
+++ src/comonents/Home/Home.js
@@ -4,8 +4,7 @@
     <img className="home_img" src="https://assets.telegraphindia.com/telegraph/77d70c6f-b19a-497e-b1ed-bc4d0ae2722b.jpg" alt="home" />
 
   <h1>MUSIC LiBARY</h1>
-  <p>If you wanna save your own albums somewhere
-  you are in a right place.
+  <p>If you need safe your favorite albums somewhere.You are in a right place.
   </p>
   </section>
 }
--- src/comonents/Home/Home.test.js
+++ src/comonents/Home/Home.test.js
@@ -3,6 +3,12 @@ import Home from "./Home"
 
 test("Home render corectly",()=>{
     render(<Home/>)
-   const textElement =screen.getByText(/music libarynp/i)
-   expect(textElement).toBeInTheDocument() 
+    const textElement =screen.getByAltText(/home/i)
+    expect(textElement).toBeInTheDocument() 
+
+   const textElement2 =screen.getByText(/music libary/i)
+   expect(textElement2).toBeInTheDocument() 
+
+   const textElement3 =screen.getByText(/If you need safe your favorite albums somewhere.You are in a right place./i)
+   expect(textElement3).toBeInTheDocument() 
 })
